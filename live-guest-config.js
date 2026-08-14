59a3fdb62b1d172df134c6d4f9e2f6093d8cc77f
WZ <3dudes1life@gmail.com>
2026-08-14 13:57:51 -0700

    Update live-guest-config.js

--- live-guest-config.js
+++ live-guest-config.js
@@ -4,10 +4,15 @@
   Example: https://throuple-tea-signal.YOUR-SUBDOMAIN.workers.dev
 */
 window.TT_LIVE_GUEST_CONFIG = {
-  signalingBaseUrl: "",
+  signalingBaseUrl: "https://throuple-tea-live-guest-signal.round-disk-6577.workers.dev",
   rtcConfig: {
     iceServers: [
-      { urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] }
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302"
+        ]
+      }
     ]
   }
 };
